62c079e674a59216ed93a38f40179f4ddc39ff1e
HM <hieumdd@gmail.com>
2024-01-18 21:50:59 +0700

    feat: Update loggings format

--- src/logging.service.ts
+++ src/logging.service.ts
@@ -3,6 +3,9 @@ import stringify from 'safe-stable-stringify';
 
 export const logger = createLogger({
     level: 'debug',
-    format: format.combine(format.printf(({ level, ...message }) => stringify({ severity: level, ...message })!)),
+    format: format.printf(({ level, message }) => {
+        const { httpRequest = undefined, ...rest } = message instanceof Object ? message : { message };
+        return stringify({ severity: level, message: rest, httpRequest })!;
+    }),
     transports: [new transports.Console()],
 });
