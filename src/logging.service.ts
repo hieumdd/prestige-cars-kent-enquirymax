9c18872b1fdbb7cf1e14b1eaff7d8d56962530a3
HM <hieumdd@gmail.com>
2024-01-18 22:19:23 +0700

    fix: Fix loggings

--- src/logging.service.ts
+++ src/logging.service.ts
@@ -4,7 +4,8 @@ import stringify from 'safe-stable-stringify';
 export const logger = createLogger({
     level: 'debug',
     format: format.printf(({ level, message }) => {
-        return stringify({ severity: level, message })!;
+        const _message = message instanceof Object ? message : { message };
+        return stringify({ severity: level, ..._message })!;
     }),
     transports: [new transports.Console()],
 });
